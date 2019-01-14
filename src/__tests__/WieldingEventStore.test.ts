31538e5b648c175dfbb9f0d7dc964ebfe63ed93c
Andrew <akwebb1@gmail.com>
2019-01-13 19:31:04 -0500

    added simple event test

--- src/__tests__/WieldingEventStore.test.ts
+++ src/__tests__/WieldingEventStore.test.ts
@@ -1,7 +1,25 @@
+import { WieldingEvent } from '../WieldingEvent';
 import { WieldingEventStore } from './../WieldingEventStore';
 
+import { WieldingObserver } from '../WieldingObserver';
+
 const testStore = new WieldingEventStore('testStore');
+let eventOneRecieved = false;
+
+function onTestEventReceived(event: any): void {
+  eventOneRecieved = true;
+}
 
 test('testStore creation', () => {
   expect(testStore.name).toBe('testStore');
+});
+
+test('event one received', () => {
+  const eventOneObserver: WieldingObserver = testStore.AddObserver(onTestEventReceived, "EVENTONE");
+
+  testStore.Dispatch(new WieldingEvent("EVENTONE", { received: true }))
+  
+  expect(eventOneRecieved).toBeTruthy();
+
+  testStore.RemoveObserver(eventOneObserver);
 });
